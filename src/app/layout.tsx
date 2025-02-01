2a0eb6b1866921debf0f70502172c2398f935f89
Pascal Gottwald <71701284+Passey59@users.noreply.github.com>
2025-02-01 22:25:01 +0100

    Feature/development/landingpage (#2) (#3)
    * added button component
    
    * added landing page with header and footer components
    
    * added logo and background animation
    
    * working on landing page, changed bg gradient colors
    
    ---------
    
    Co-authored-by: Robert Ackermann <robert_ackermann@icloud.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import type React from "react"; // Added import for React
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Minimalistic Landing Page",
+    description: "A minimalistic landing page with sharing is caring theme",
+};
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="de" suppressHydrationWarning>
+            <body className={inter.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="light"
+                    enableSystem={false}
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
         </html>
     );
 }
