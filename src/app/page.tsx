d06677e4a622c97e7066cd51d9edfdf3256fc1bc
Passey59 <pascalgottwald@t-online.de>
2025-02-01 20:06:14 +0100

    added landing page with header and footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@/components/ui/button";
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Home() {
     return (
-        <div>
-            <Button>Los gehts</Button>
+        <div className="min-h-screen flex flex-col relative overflow-hidden">
+            <BackgroundGradientAnimation />
+            <Header />
+            <main className="flex-grow flex flex-col items-center justify-center z-10">
+                <h1 className="text-4xl font-bold mb-8">Sharing is Caring</h1>
+                <Button size="lg">Login / Register</Button>
+            </main>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { ModeToggle } from "../ui/mode-toggle";
+
+export default function Header() {
+    return (
+        <header className="w-full p-4 flex justify-between items-center z-10">
+            <div className="flex items-center">
+                <Image src="/logo.svg" alt="App Logo" width={40} height={40} />
+            </div>
+            <ModeToggle />
+        </header>
+    );
+}
